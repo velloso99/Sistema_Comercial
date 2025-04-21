5667d924a0b4bce4fc3e5570f90133daebd51c5c
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-21 19:26:43 -0300

    prjinfox

---
+++ prjinfox/src/main/java/com/mycompany/prjinfox/Prjinfox.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjinfox;
+
+/**
+ *
+ * @author vello
+ */
+public class Prjinfox {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
